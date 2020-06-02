16ab1b60011e538eabd805fa3948f1810631f5cc
Adrijan Bajrami <abajrami@everhype-systems.eu>
2020-06-03 01:49:40 +0200

    + Get auth code + Get auth info

--- lumaserv_domain_api/domain.py
+++ lumaserv_domain_api/domain.py
@@ -30,3 +30,32 @@ def get_domain(api_token, domain):
     })
 
     return r.json()
+
+
+def get_auth_info(api_token, domain):
+    """
+    RETURNS INFORMATION ABOUT THE AUTH CODE
+    :param domain: DECLARES THE DOMAIN TO BE USED
+    :param api_token: DECLARES THE API TOKEN
+    :return: RETURNS LIST
+    """
+
+    r = requests.post(base.format(endpoint="domain/domains/authcode", API_TOKEN=api_token), params={
+        "domainName": domain
+    })
+
+    return r.json()
+
+
+def get_auth_code(api_token, domain):
+    """
+       RETURNS THE AUTH CODE FOR ONE DOMAIN
+       :param domain: DECLARES THE DOMAIN TO BE USED
+       :param api_token: DECLARES THE API TOKEN
+       :return: RETURNS LIST
+       """
+
+    return get_auth_info(api_token, domain)["data"]["domain"]["authinfo"]
+
+
+
