f5ae4e918788904f2c4ca68d877ee32d4c2a6531
Adrijan Bajrami <abajrami@everhype-systems.eu>
2020-06-03 01:40:46 +0200

    + Get one Domain

--- lumaserv_domain_api/domain.py
+++ lumaserv_domain_api/domain.py
@@ -15,3 +15,18 @@ def get_domains(api_token):
     r = requests.get(base.format(endpoint="domain/domains", API_TOKEN=api_token))
 
     return r.json()
+
+
+def get_domain(api_token, domain):
+    """
+    RETURNS INFORMATION ABOUT ONE DOMAIN
+    :param domain: DECLARES THE DOMAIN TO BE USED
+    :param api_token: DECLARES THE API TOKEN
+    :return: RETURNS LIST
+    """
+
+    r = requests.get(base.format(endpoint="domain/domains/show", API_TOKEN=api_token), params={
+        "domainName": domain
+    })
+
+    return r.json()
