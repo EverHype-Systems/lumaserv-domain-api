0a8de5b71e14687dfe9750d424ef7a3b7650c5aa
Adrijan Bajrami <abajrami@everhype-systems.eu>
2020-06-03 14:51:07 +0200

    Completed Contact/Handle

--- lumaserv_domain_api/contact.py
+++ lumaserv_domain_api/contact.py
@@ -15,3 +15,129 @@ def get_handles(api_token):
 
     return r.json()
 
+
+def get_handle(api_token, handle):
+    """
+    RETURNS ONE DOMAIN HANDLE
+    :param handle: HANDLE ID
+    :param api_token: DECLARES THE API TOKEN
+    :return: RETURNS DECODED LIST
+    """
+
+    r = requests.get(base.format(endpoint="domain/handles/show", API_TOKEN=api_token), params={
+        "handle": handle
+    })
+
+    return r.json()
+
+
+
+def get_countries(api_token):
+    """
+    RETURNS ALL AVAILABLE COUNTRIES
+    :param api_token: DECLARES THE API TOKEN
+    :return: RETURNS DECODED LIST
+    """
+
+    r = requests.get(base.format(endpoint="domain/handles/countries", API_TOKEN=api_token))
+
+    return r.json()
+
+
+def create_handle(api_token, type, sex, firstname, lastname, organisation, street, number, postcode, city, region,
+                  country, email, countryofbirth=None, user=None, dateofbirth=None):
+    """
+    CREATES A DOMAIN CONTACT
+    :return:
+    :param api_token:
+    :param type:
+    :param sex:
+    :param firstname:
+    :param lastname:
+    :param organisation:
+    :param street:
+    :param number:
+    :param postcode:
+    :param city:
+    :param region:
+    :param country:
+    :param email:
+    :param countryofbirth: OPTIONAL
+    :param user: OPTIONAL
+    :param dateofbirth: OPTIONAL
+    :return:
+    """
+
+    r = requests.post(base.format(endpoint="domain/handles/create", API_TOKEN=api_token), params={
+        "type": type,
+        "sex": sex,
+        "firstname": firstname,
+        "lastname": lastname,
+        "organisation": organisation,
+        "street": street,
+        "number": number,
+        "postcode": postcode,
+        "city": city,
+        "region": region,
+        "country": country,
+        "email": email,
+        "countryofbirth": countryofbirth,
+        "user": user,
+        "dateofbirth": dateofbirth
+    })
+
+    return r.json()
+
+
+def delete_handle(api_token, handle):
+    """
+    DELETE A CREATED CONTACT/HANDLE
+    :param api_token:
+    :param handle:
+    :return:
+    """
+
+    r = requests.delete(base.format(endpoint="domain/handles/delete", API_TOKEN=api_token), params={
+        "handle": handle
+    })
+
+    return r.json()
+
+
+def edit_handle(api_token, handle, organisation=None, street, number, postcode, city, region, country, email,
+                countryofbirth=None, user=None):
+    """
+    EDIT A CREATED CONTACT/HANDLE
+    :param api_token:
+    :param handle:
+    :param organisation:
+    :param street:
+    :param number:
+    :param postcode:
+    :param city:
+    :param region:
+    :param country:
+    :param email:
+    :param countryofbirth:
+    :param user:
+    :return:
+    """
+
+    r = requests.post(base.format(endpoint="domain/handles/edit", API_TOKEN=api_token), params={
+        "handle": handle,
+        "organisation": organisation,
+        "street": street,
+        "number": number,
+        "postcode": postcode,
+        "city": city,
+        "region": region,
+        "country": country,
+        "email": email,
+        "countryofbirth": countryofbirth,
+        "user": user
+    })
+
+    return r.json()
+
+
+
