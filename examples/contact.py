410ae9b59f94c5617e4144025b67ba0aa3144aea
Adrijan Bajrami <abajrami@everhype-systems.eu>
2020-06-04 11:08:47 +0200

    || Recode the example for contacts

--- examples/contact.py
+++ examples/contact.py
@@ -1,23 +1,25 @@
-import lumaserv_domain_api.contact as contact
+from lumaserv_domain_api.contact import Contact
 
 # DEFINE YOUR API KEY
 apikey = "PutYourKeyHere"
 
+contact = Contact(apikey)
+
 # FETCH ALL YOUR CREATED CONTACTS
-print(contact.get_handles(apikey))
+print(contact.get_handles())
 
 # FETCH INFORMATION ABOUT ONE CONTACT
 # PUT THE HANDLE ID AS 2. PARAM
-print(contact.get_handle(apikey, "ABCDEF"))
+print(contact.get_handle("ABCDEF"))
 
 # GET ALL AVAILABLE COUNTRIES
-print(contact.get_countries(apikey))
+print(contact.get_countries())
 
 # DELETE HANDLE/CONTACT
-print(contact.delete_handle(apikey, "ABCDEF"))
+print(contact.delete_handle("ABCDEF"))
 
 # EDIT A CONTACT/HANDLE
-print(contact.edit_handle(apikey, "ABCDEF", "Street", 11, "36037", "Fulda", "Hessen", "DE", "customer@service.com"))
+print(contact.edit_handle("ABCDEF", "Street", 11, "36037", "Fulda", "Hessen", "DE", "customer@service.com"))
 
 
 
