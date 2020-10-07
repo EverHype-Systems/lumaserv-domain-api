6b05eaf935ec8906141ca47502be1639d9007ccd
Adrijan Bajrami <abajrami@everhype-systems.eu>
2020-10-07 22:00:15 +0200

    update endpoint

--- lumaserv_domain_api/zones.py
+++ lumaserv_domain_api/zones.py
@@ -36,7 +36,7 @@ class DomainZones:
         :return:
         """
 
-        r = requests.get(self.base.format(endpoint="/dns/zones/records/create", API_TOKEN=self.api_token), params={
+        r = requests.get(self.base.format(endpoint="/dns/zones/records/add", API_TOKEN=self.api_token), params={
             "zone": zone,
             "records": records,
         })
